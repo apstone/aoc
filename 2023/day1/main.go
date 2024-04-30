d984c343e440d85bc7bfe4229aca5e4b5bf641bd
Aaron Stone <apstone@protonmail.com>
2024-04-30 19:24:40 -0400

    part 2

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -1,44 +1,80 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	input, err := os.ReadFile("./input.txt")
-	if err != nil {
-		panic(err)
+	findCount("input.txt")
+}
+
+func findCount(filename string) {
+	bytes, _ := os.ReadFile(filename)
+	fileContent := string(bytes)
+
+	replacements := [][]string{
+		{"one", "1"},
+		{"two", "2"},
+		{"three", "3"},
+		{"four", "4"},
+		{"five", "5"},
+		{"six", "6"},
+		{"seven", "7"},
+		{"eight", "8"},
+		{"nine", "9"},
 	}
 
-	lines := bytes.Split(input, []byte("\n"))
+	lines := strings.Split(fileContent, "\n")
+	re := regexp.MustCompile("[0-9]")
 
-	// Create a regex for finding numbers
-	re, err := regexp.Compile("[0-9]")
-	if err != nil {
-		panic(err)
-	}
+	sum := 0
+	for _, line := range lines {
+		words := getWords(replacements, line)
 
-	totalValue := 0
-
-	for i := 0; i < len(lines); i++ {
-		line := lines[i]
-		if len(line) > 0 {
-			numMatches := re.FindAllString(string(line), -1)
-			firstDigit := numMatches[0]
-			lastDigit := numMatches[len(numMatches)-1]
-			mergedDigits := firstDigit + lastDigit
-
-			number, err := strconv.Atoi(mergedDigits)
-			if err != nil {
-				panic(err)
-			}
-			totalValue += number
+		digits := re.FindAllString(line, -1)
+		if len(digits) <= 0 {
+			continue
 		}
 
+		first := digits[0]
+		last := digits[len(digits)-1]
+
+		if len(words) > 0 {
+			firstWord := replacements[words[0]]
+			if strings.Index(line, firstWord[0]) < strings.Index(line, first) {
+				first = firstWord[1]
+			}
+
+			lastWord := replacements[words[len(words)-1]]
+			if strings.LastIndex(line, lastWord[0]) > strings.LastIndex(line, last) {
+				last = lastWord[1]
+			}
+		}
+
+		crc := fmt.Sprintf("%s%s", first, last)
+		num, err := strconv.Atoi(crc)
+		if err != nil {
+			continue
+		}
+		sum += num
 	}
-	fmt.Println(totalValue)
+
+	fmt.Printf("Sum is %d", sum)
+}
+
+func getWords(replacements [][]string, line string) []int {
+	words := []int{}
+	for i := 0; i < len(line); i++ {
+		for id, replacement := range replacements {
+			if strings.HasPrefix(line[i:], replacement[0]) {
+				words = append(words, id)
+			}
+		}
+	}
+
+	return words
 }
